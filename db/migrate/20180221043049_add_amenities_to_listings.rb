7ef6b831bdfe16a652999f2fd9b5f734d014e7e0
Kok Yee Yang <yee_yang94@hotmail.com>
2018-03-01 15:15:36 +0800

    remove user id column

--- db/migrate/20180221043049_add_amenities_to_listings.rb
+++ db/migrate/20180221043049_add_amenities_to_listings.rb
@@ -1,6 +1,5 @@
 class AddAmenitiesToListings < ActiveRecord::Migration[5.0]
   def change
     add_column :listings, :amenities, :string, array: true, default: []
-    add_reference :listings, :user, foreign_key: true, index: true
   end
 end
