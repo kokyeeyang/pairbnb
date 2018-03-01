45834cfd19772999c3bf5e9608cedef2b8dff632
Kok Yee Yang <yee_yang94@hotmail.com>
2018-03-01 15:06:53 +0800

    edit a lot

--- db/migrate/20180221043049_add_amenities_to_listings.rb
+++ db/migrate/20180221043049_add_amenities_to_listings.rb
@@ -1,6 +1,6 @@
 class AddAmenitiesToListings < ActiveRecord::Migration[5.0]
   def change
     add_column :listings, :amenities, :string, array: true, default: []
-    add_references :listings, :user, foreign_key: true, index: true
+    add_reference :listings, :user, foreign_key: true, index: true
   end
 end
